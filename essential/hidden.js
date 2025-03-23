9320f2acf66091195c5703e5edd08a066e762500
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2025-03-23 15:57:31 -0400

    Add files via upload

---
+++ essential/hidden.js
@@ -0,0 +1,22 @@
+const main = "U2FsdGVkX1+tjXqD8U4szWubd0Knxe5lE68T2vSE5R1GckgU6e74VgT/AAWf7GejD6N3tqg+k7F/poZ3F4e1K70wHmS2cUrFkAIk8J2p6gY=";
+const key = 'UIqWDlOhgP47V02P47WDlOhgfWVN9cdsvUcdsvU';
+
+function encryptPassword(userinput) {
+    const encrypted = CryptoJS.AES.encrypt(userinput, key).toString();
+    return encrypted;
+}
+
+// Add event listener for the button click
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("submitButton").addEventListener("click", function() {
+        const userinput = document.getElementById("passwordInput").value;
+        const enc = encryptPassword(userinput);
+
+        // Compare the encrypted user input to the pre-encrypted password
+        if (enc === main) {
+            window.location.href = "https://drive.google.com/uc?export=download&id=102P47VkNANWUSUIqWDlOhgfWVN9cdsvU";
+        } else {
+            alert("Incorrect password. Please try again.");
+        }
+    });
+});
