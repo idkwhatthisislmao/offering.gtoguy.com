486cc75da7a8054a918865277facd2527f339b64
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2025-03-23 16:25:26 -0400

    Update hidden.js

--- essential/hidden.js
+++ essential/hidden.js
@@ -1,8 +1,13 @@
-const main = "U2FsdGVkX19hnsxPn2BJLuyhPtBPSnFMMu6W7NJ8arQ=";
+const main = "jS50aQyWq7Q3PrKxG/tCPw===";
 const key = 'UIqWDlOhgP47V02P47WDlOhgfWVN9cdsvUcdsvU';
 
 function encryptPassword(userinput) {
-    const encrypted = CryptoJS.AES.encrypt(userinput, key).toString();
+    const iv = CryptoJS.enc.Utf8.parse('0000000000000000');
+    const encrypted = CryptoJS.AES.encrypt(userinput, CryptoJS.enc.Utf8.parse(key), {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    }).toString();
     return encrypted;
 }
 
@@ -10,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("submitButton").addEventListener("click", function() {
         const userinput = document.getElementById("passwordInput").value;
         const enc = encryptPassword(userinput);
-        
+
         if (enc === main) {
             alert("corect");
         } else {
