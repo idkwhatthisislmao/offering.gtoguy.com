fcc59d10b17eda0c114c9d154b55eff68f26cdf0
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2025-03-23 16:14:31 -0400

    Update hidden.js

--- essential/hidden.js
+++ essential/hidden.js
@@ -1,4 +1,4 @@
-const main = "U2FsdGVkX1+tjXqD8U4szWubd0Knxe5lE68T2vSE5R1GckgU6e74VgT/AAWf7GejD6N3tqg+k7F/poZ3F4e1K70wHmS2cUrFkAIk8J2p6gY=";
+const main = "IraL1/66fQ1JbnWssFxNWg==";
 const key = 'UIqWDlOhgP47V02P47WDlOhgfWVN9cdsvUcdsvU';
 
 function encryptPassword(userinput) {
@@ -10,6 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("submitButton").addEventListener("click", function() {
         const userinput = document.getElementById("passwordInput").value;
         const enc = encryptPassword(userinput);
+
+        console.log(enc);
         
         if (enc === main) {
             window.location.href = "https://drive.google.com/uc?export=download&id=102P47VkNANWUSUIqWDlOhgfWVN9cdsvU";
